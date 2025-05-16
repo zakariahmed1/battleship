ce31e925d9934219a084359206945ed94917175a
zakariahmed1 <163400596+zakariahmed1@users.noreply.github.com>
2025-05-16 16:20:40 +0200

    adding various methods to cell

--- src/main/java/application/Cell.java
+++ src/main/java/application/Cell.java
@@ -1,5 +1,22 @@
 package application;
 
 public class Cell {
+    Ship ship;
+    boolean attacked ;
 
+    public void placeShip(Ship ship){
+        this.ship = ship;
+    }
+    public boolean hasShip(){
+
+        return ship!=null;
+    }
+    public boolean isAttacked(){
+        return true;
+    }public void attack ( ){
+    }
+    public Ship getShip(){
+        return ship;
+
+    }
 }
