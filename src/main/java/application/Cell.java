75625dd09a4a21e6d66773d269fd93bdaf8fea79
zakariahmed1 <163400596+zakariahmed1@users.noreply.github.com>
2025-05-28 09:03:13 +0200

    setting up board and cell

--- src/main/java/application/Cell.java
+++ src/main/java/application/Cell.java
@@ -2,21 +2,53 @@ package application;
 
 public class Cell {
     Ship ship;
-    boolean attacked ;
+    int x;
+    int y;
+    boolean attacked;
 
-    public void placeShip(Ship ship){
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.attacked = false;
+    }
+
+    public Cell() {
+        this.attacked = false;
+    }
+
+    public void placeShip(Ship ship) {
         this.ship = ship;
     }
-    public boolean hasShip(){
 
-        return ship!=null;
+    public boolean hasShip() {
+        return ship != null;
     }
-    public boolean isAttacked(){
-        return true;
-    }public void attack ( ){
+
+    public boolean isAttacked() {
+        return attacked;
     }
-    public Ship getShip(){
+
+    public void attack() {
+        this.attacked = true;
+        if (hasShip()) {
+            ship.registerHit(this);
+        }
+    }
+
+    public Ship getShip() {
         return ship;
+    }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Cell cell = (Cell) obj;
+        return x == cell.x && y == cell.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * x + y;
     }
 }
